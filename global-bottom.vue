ac78424006f667e0e1a21b6941027c115e138848
Pasha Finkelshteyn <pavel.finkelshtein@gmail.com>
2023-11-23 08:19:39 +0200

    Update global-bottom.vue

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,4 +1,4 @@
 <template>
-  <footer v-if="$page != 1 && $page != 25" class="absolute bottom-0 left-0 right-0 p-2"><logos-twitter/><a href="https://linktr.ee/asm0dey">asm0dey</a></footer>
+  <footer v-if="$page != 1 && $page != 25" class="absolute bottom-0 left-0 right-0 p-2"><logos-twitter/><a href="https://linktr.ee/asm0dey">asm0di0</a></footer>
 </template>
 
